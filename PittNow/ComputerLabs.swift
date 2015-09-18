b1c260c59d728411eb9d3834399e0a41d3f4618b
Omri Barak <omribarak@pitt.edu>
2015-09-17 23:59:59 -0400

    Added extra view and prototype for JSON requests

--- PittNow/ComputerLabs.swift
+++ PittNow/ComputerLabs.swift
@@ -9,9 +9,33 @@
 import UIKit
 
 class ComputerLabs: UIViewController {
-
+    @IBOutlet weak var myLabel: UILabel!
+    func setMessage(text: String) {
+        self.myLabel.text = text
+    }
+    func test() {
+        var url : String = "http://pittapi.pittcsc.org/"
+        var urlRequest = NSURLRequest(URL: NSURL(string: url)!)
+        
+        NSURLConnection.sendAsynchronousRequest(urlRequest, queue: NSOperationQueue(),
+            completionHandler:{ (response:NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+                if(data != nil && error==nil) {
+//                    let data = String(format: data, NSUTF8StringEncoding)
+                    dispatch_async(dispatch_get_main_queue()) {
+                        self.setMessage(/*data*/"Yeah baby")
+                    }
+                } else {
+                    dispatch_async(dispatch_get_main_queue()) {
+                        self.setMessage("Error")
+                        println(error.description)
+                    }
+                }
+        })
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.myLabel.text = "Proof"
+        self.test()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
