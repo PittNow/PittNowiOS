a0e9b78ecf662bf83080a3c13472c08a85a295ea
Omri Barak <actualhater@cl-130-49-7-21.wireless.pitt.edu>
2015-09-17 22:29:17 -0400

    Initial Commit

---
+++ PittNow/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  PittNow
+//
+//  Created by Omri Barak on 9/17/15.
+//  Copyright (c) 2015 PittNow. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
